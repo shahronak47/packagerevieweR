94e9ee944f929911aa36190f12524f349f50413e
Ronak Shah <shahronak47@yahoo.in>
2023-04-09 11:37:23 +0800

    correct ui

--- ui.R
+++ ui.R
@@ -4,8 +4,17 @@ fluidPage(
       column(10, titlePanel("Package Reviewer")),  
       column(2, div(class = "sign_up_btn", actionButton("sign_up", "Sign Up!")))
     ),
-    selectizeInput('selected_package', 'Select a package to view the reviews.', choices = NULL),
-    shinyRatings('ratings'), 
-    textOutput('text'), 
-    actionButton('submit', 'Submit')
+    sidebarLayout(
+      sidebarPanel(
+            textInput('username', 'Username/Email'), 
+            passwordInput('password', 'Password', placeholder = 'Enter your password')
+      ), 
+      mainPanel(
+             selectizeInput('selected_package', 'Select a package to view the reviews.', choices = NULL),
+             shinyRatings('ratings'), 
+             textOutput('text'), 
+             textAreaInput("review", "Add your Review (optional)"),
+             actionButton('submit', 'Submit')
+      )  
+    )
 )
