b91a85ea3e005a722a3b53bb3dc8ede45992469c
Ronak Shah <shahronak47@yahoo.in>
2023-12-23 21:14:53 +0800

    add id for sidebar

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ page_sidebar(
     shinyjs::useShinyjs(),
     shiny::includeCSS('www/stylesheet.css'),
     theme = bs_theme(bootswatch = "solar"),
-    sidebar = sidebar(
+    sidebar = sidebar(id = "login",
             textInput('username', 'Username'), 
             passwordInput('password', 'Password', placeholder = 'Enter your password'), 
             actionButton('login_btn', 'Login'), 
@@ -12,10 +12,10 @@ page_sidebar(
             div(class = "sign_up_btn", id = "sign_up_btn", actionButton("sign_up", "Sign Up!")),
       ), 
       card(
-             selectizeInput('selected_package', 'Select a package to view the reviews.', choices = NULL),
-             shinycssloaders::withSpinner(uiOutput("avg_box")),
-             uiOutput('your_review'), 
-             h3("User Reviews : "),
-             shinycssloaders::withSpinner(dataTableOutput('review_table'))
+         selectizeInput('selected_package', 'Select a package to view the reviews.', choices = NULL),
+         shinycssloaders::withSpinner(uiOutput("avg_box")),
+         uiOutput('your_review'), 
+         h3("User Reviews : "),
+         shinycssloaders::withSpinner(dataTableOutput('review_table'))
       )  
   )
