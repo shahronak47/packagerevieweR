a14183907bfd302af5649be3b86c50bfc2b4bef5
shahronak47 <shahronak47@yahoo.in>
2024-08-14 22:28:36 +0530

    update text

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # packagerevieweR
 
+This is an R shiny app for reviewing R packages. The app is very simple to use. Viewing the reviews does not require any login however, if you want to post a review you need to sign up with your email address. 
+
+R has more than 20k packages now. Some packages like `dplyr`, `data.table`, `ggplot2` etc are very well known and are reliable. Most R developers know and trust them. However, there are some packages which are very specific, they do a very specific thing. Sometimes there are also multiple packages which do the same thing. 
+
+It can be difficult to choose one package which suits your needs perfectly when you have not heard about that package before. You don’t know which one to choose. There’s no way to compare them or get some feedback. Do I trust this package? Is it reliable? Are there any security issues with the package? What is the opinion of other developers who have used this package before? What do they think? 
+
+I am trying to answer all these questions with this project called Package Reviewer.
+
+Goal of package reviewer is to be a review site where people can review the packages that they have used. They can assign 0.5-5 stars to the package and add reviews in text form. It is Google reviews for packages.
+      
 Application is deployed at https://shahronak.shinyapps.io/packagerevieweR/
--- global.R
+++ global.R
@@ -67,14 +67,14 @@ about_text <- function() {
   tagList(
     p("R has more than 20k packages now. Some packages like dplyr, data.table, ggplot2 etc are very well known and are reliable. Most R developers know and trust them."),
     p("However, there are some packages which are very specific, they do a very specific thing. 
-    Sometimes there are also multiple packages who do the same thing. 
+    Sometimes there are also multiple packages which do the same thing. 
     It can be difficult to choose one package which suits your needs perfectly when you have not heard about that package before. 
     You don’t know which one to choose. There’s no way to compare them or get some feedback. 
     Do I trust this package? Is it reliable? Are there any security issues with the package? 
     What is the opinion of other developers who have used this package before? What do they think? 
     I am trying to answer all these questions with my project called Package Reviewer."),
     p("Goal of package reviewer is to be a review site where people can review the packages that they have used. 
-      They can assign 0.5-5 stars to the package and add reviews in text form. It is google reviews for packages.
+      They can assign 0.5-5 stars to the package and add reviews in text form. It is Google reviews for packages.
       Viewing reviews does not need an account. However, if you want to post a review then you need to create an account by sharing your email address.")
   )
 }
@@ -91,7 +91,7 @@ next_text <- function() {
             </ul>
           </p>
          "), 
-    p("If you think there is something else that can be useful here, feel free to email me at shahronak47[at]yahoo[dot]in")
+    shiny::HTML("<p>If you think there is something else that can be useful here, feel free to <a href = 'https://github.com/shahronak47/packagerevieweR/issues/new'>create an issue on the GitHub repo.</a></p>")
   )
 }
 
