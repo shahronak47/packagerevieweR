6149530b46ed17835a55316450b95f8f2f114bbf
Ronak Shah <shahronak47@yahoo.in>
2025-12-30 18:11:14 +0530

    change hardcoded to environment variable

--- cypress/e2e/3-packageReviewer/test-packageReviewer.cy.js
+++ cypress/e2e/3-packageReviewer/test-packageReviewer.cy.js
@@ -3,8 +3,8 @@ context('Tests', () => {
       cy.visit('https://shahronak.shinyapps.io/packagerevieweR/')
 })
     it('Can login succesfully in the app', () => {
-        cy.get('#username').type('shahronak47');
-        cy.get('#password').type('test');
+        cy.get('#username').type(Cypress.env('APP_USERNAME'));
+        cy.get('#password').type(Cypress.env('APP_PASSWORD'));
         cy.get('#login_btn').click();
         cy.get('#selected_package-label').should('have.text', 'Select a package to view the reviews.');
     })
